dfbba717d3bbee5764b2b7e2467bc6f10d2d18c4
Arnav Kulkarni <akulkarni34587@gmail.com>
2025-09-27 19:05:04 -0400

    [fix] Typp in about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -112,7 +112,7 @@ export default function Home() {
                   I&apos;m mainly interested in distributed systems, but my previous internship at Grepr got me hooked on 
                   stateful, fault-tolerant, stream processing engines. I also am interested in integrating AI/ML systems into already established SaaS products. 
                   In my free time, I play and watch soccer(Arsenal fan unforunately), lift weights,
-                  and attempt to improve my reading and writing skills in a world where AI is growing and owning original thought is important.
+                  and attempt to improve my reading and writing skills in a world where AI is growing and owning original thought is so valuable.
                 </p>
               </div>
             </section>
