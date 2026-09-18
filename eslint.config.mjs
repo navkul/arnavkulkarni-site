466e7f252a876e1b476fb24895f55addd3902297
Arnav Kulkarni <akulkarni34587@gmail.com>
2026-09-18 13:52:29 -0400

    Upgrade Next.js to 16.3.3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,12 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 
 const eslintConfig = [
   {
     ignores: [".next/**", "next-env.d.ts"],
   },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...nextVitals,
+  ...nextTypescript,
 ];
 
 export default eslintConfig;
